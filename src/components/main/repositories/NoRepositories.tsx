8b76629eddb90666fa8ff37e9a9f735438515e98
Nikolay <limnikolay99@gmail.com>
2022-04-24 14:43:15 +0300

    create noRepositories layout

--- src/components/main/repositories/NoRepositories.tsx
+++ src/components/main/repositories/NoRepositories.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import style from './NoRepositories.module.css'
+import noRepoIcon from "../../../icons/noRepo.svg";
 
 const NoRepositories = () => {
     return (
         <div className={style.no_repo}>
-            
+                <div className={style.no_repo_element}>
+                    <div className={style.no_repo_icon}><img src={noRepoIcon} alt="noRepoIcon"/></div>
+                    <div className={style.text_style}><span >Repository list is empty</span></div>
+                </div>
         </div>
     );
 };
