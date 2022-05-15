a7db6dba081a79f108b96cd07b832d7e59764d12
Nikolay <limnikolay99@gmail.com>
2022-05-15 15:58:32 +0300

    fix followers/following

--- src/components/main/profile/Profile.tsx
+++ src/components/main/profile/Profile.tsx
@@ -9,6 +9,9 @@ import React from "react";
 const Profile = () => {
     const state = useSelector<AppRootStateType, ProfileStateType>((state) => state.profile)
 
+    const followers = Number(state.profile.followers) >= 1000 ? (Number(state.profile.followers) / 1000).toFixed(1) + "k" : state.profile.followers
+    const following = Number(state.profile.following) >= 1000 ? (Number(state.profile.following) / 1000).toFixed(1) + "k" : state.profile.following
+
     return (
         <div className={style.profile_container}>
             <div className={style.profile}>
@@ -21,10 +24,10 @@ const Profile = () => {
                                                                 rel="noreferrer">{state.profile.login}</a></div>
                     <div className={style.profile_info_follow}>
                         <div className={style.profile_info_follow_followers}>
-                            <img src={followersIcon} alt="followers"/><span>{state.profile.followers} followers</span>
+                            <img src={followersIcon} alt="followers"/><span>{followers} followers</span>
                         </div>
                         <div className={style.profile_info_follow_following}>
-                            <img src={followingIcon} alt="following"/><span>{state.profile.following} following</span>
+                            <img src={followingIcon} alt="following"/><span>{following} following</span>
                         </div>
                     </div>
                 </div>
